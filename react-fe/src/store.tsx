1200466c3234302ca555ffcbc9a0940811ad71f2
Ishaq Yousef Haj Hasan <ishaqyousefhajhasan@Ishaqs-MacBook-Pro.local>
2023-03-22 17:31:05 -0700

    setup user and auth store for app

--- react-fe/src/store.tsx
+++ react-fe/src/store.tsx
@@ -1,13 +1,35 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+import { UserType } from './types/user'
 
 export interface AuthState {
     isAuthenticated: boolean,
+    token: string | null | undefined
     login: () => void,
     logout: () => void,
 }
 
-export const authStore = create<AuthState>()((set) => ({
-    isAuthenticated: false,
-    login: () => set(() => ({isAuthenticated: true})),
-    logout: () => set(() => ({isAuthenticated: false})),
-  }))
+export interface UserStoreType {
+    user: UserType | null | undefined,
+}
+
+export const authStore = create<AuthState>()(
+    persist(
+        (set) => ({
+            isAuthenticated: false,
+            token: null,
+            login: () => set(() => ({isAuthenticated: true})),
+            logout: () => set(() => ({isAuthenticated: false, token: null})),
+        }),
+        {
+            name: 'auth-storage'
+        }
+    )
+)
+
+export const userStore = create<UserStoreType>()(
+    persist((set) => ({
+        user: null
+    }), {name: 'user-storage'})
+)
