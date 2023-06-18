16eac7fc452e7d7029623c03407a1addd4f5c156
Ishaq Yousef HajHasan <ishaq.y.hajhasan@gmail.com>
2023-06-17 19:40:19 -0700

    Add route for admin to create_user accounts

--- fastapi_be/app/main.py
+++ fastapi_be/app/main.py
@@ -19,7 +19,6 @@ app.include_router(auth.router, prefix="/auth")
 
 app.include_router(users.router, prefix="/users", dependencies=[Depends(get_current_user)])
 
-# TODO: Add dependency that user is admin
 app.include_router(admins.router, prefix="/admins", dependencies=[Depends(get_current_admin)])
 
 @app.get('/')
--- fastapi_be/app/routers/admins.py
+++ fastapi_be/app/routers/admins.py
@@ -5,12 +5,21 @@ import app.models as models
 from app.utils import  authentication
 from datetime import datetime, timedelta
 from app.form_types.login_form  import LoginForm
+from app.exception.client_error import InvalidPayloadException
 
 router = APIRouter()
 
 @router.get("/me/", response_model=schemas.AdminUser)
 async def read_users_me(current_user: models.User = Depends(authentication.get_current_admin)):
     return schemas.user_to_admin_user(current_user)
+
+@router.post("/create-user", response_model=schemas.User)
+def create_user(
+    user: schemas.UserCreate, db: Session = Depends(db.get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise InvalidPayloadException('Email is already registered')
+    return crud.create_user(db=db, user=user)
 
 @router.get("/count-metrics")
 async def table_metrics(db: Session = Depends(db.get_db)):
